5447ebac745f11f3763101d411fcafcadf020ac1
itsmechauhan <techhchacker@gmail.com>
2025-04-18 12:38:12 +0530

    new

--- src/components/experienceSection/ExperienceTopMiddle.jsx
+++ src/components/experienceSection/ExperienceTopMiddle.jsx
@@ -2,7 +2,10 @@
 const ExperienceTopMiddle = () => {
   return (
     <div className="lg:w-[35%] md:w-[50%] sm:w-[80%]">
-      <img src="../../public/images/experience-image.png"
+      {/* <img src="../../public/images/experience-image.png"
+       alt="my experience" />
+    */}
+      <img src="images/experience-image.png"
        alt="my experience" />
     </div>
   );
